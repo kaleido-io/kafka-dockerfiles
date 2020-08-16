bdd2195c65e8ca38e70d70ecd81cc76c7f0227f5
Staffan Olsson <staffan@repos.se>
2020-08-16 10:17:37 +0200

    Dammit scala

---
+++ native/substitutions/admincmd/src/main/java/se/repos/substitutions/kafka/admincmd/JMX.java
@@ -0,0 +1,46 @@
+package se.repos.substitutions.kafka.admincmd;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+/*
+Call path from entry point to kafka.metrics.KafkaYammerMetrics.defaultRegistry(): 
+	at kafka.metrics.KafkaYammerMetrics.defaultRegistry(KafkaYammerMetrics.java:44)
+	at kafka.metrics.KafkaMetricsGroup.removeMetric(KafkaMetricsGroup.scala:80)
+	at kafka.metrics.KafkaMetricsGroup.removeMetric$(KafkaMetricsGroup.scala:79)
+	at kafka.zk.KafkaZkClient.removeMetric(KafkaZkClient.scala:53)
+	at kafka.zk.KafkaZkClient.close(KafkaZkClient.scala:1450)
+	at kafka.admin.TopicCommand$ZookeeperTopicService.close(TopicCommand.scala:507)
+	at kafka.admin.TopicCommand$.main(TopicCommand.scala:77)
+	at kafka.admin.TopicCommand.main(TopicCommand.scala)
+	at com.oracle.svm.core.JavaMainWrapper.runCore(JavaMainWrapper.java:149)
+	at com.oracle.svm.core.JavaMainWrapper.run(JavaMainWrapper.java:184)
+	at com.oracle.svm.core.code.IsolateEnterStub.JavaMainWrapper_run_5087f5482cc9a6abc971913ece43acb471d2631b(generated:0)
+
+	at com.oracle.graal.pointsto.constraints.UnsupportedFeatures.report(UnsupportedFeatures.java:129)
+	at com.oracle.svm.hosted.NativeImageGenerator.runPointsToAnalysis(NativeImageGenerator.java:750)
+	... 8 more
+*/
+
+/*
+import com.yammer.metrics.core.MetricsRegistry;
+
+@TargetClass(className = "scala.collection.immutable.VM")
+class JMX {
+
+  @Substitute
+  public static MetricsRegistry defaultRegistry() {
+    return null;
+  }
+
+}
+*/
+
+@TargetClass(className = "kafka.metrics.KafkaMetricsGroup")
+class JMX {
+
+  @Substitute
+  public void removeMetric(String name, scala.collection.Map<String, String> tags) {
+  }
+
+}
