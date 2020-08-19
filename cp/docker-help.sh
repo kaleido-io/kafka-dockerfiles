bb505ef715ce73c449e97728fc862011779eebfc
John Eckhart <jweckhart@gmail.com>
2020-08-19 13:12:37 -0400

    Sort entries in docker help so they are printed in alphabetical order

--- cp/docker-help.sh
+++ cp/docker-help.sh
@@ -2,8 +2,8 @@
 echo "Hi,"
 echo ""
 echo "Select as entrypoint one of these scripts:"
-find ./bin/* -printf "%f\n"
+find ./bin/* -printf "%f\n" | sort
 echo ""
 echo "You might find one of the sample config files useful:"
-find /etc/ -name *.properties
+find /etc/ -name *.properties | sort
 echo ""
--- kafka/docker-help.sh
+++ kafka/docker-help.sh
@@ -5,10 +5,10 @@ echo "This image is basically just the official Kafka distribution,"
 echo "containing both servers and utils, each with its own help output."
 echo ""
 echo "Select as entrypoint one of these scripts:"
-find ./bin/ -name *.sh
+find ./bin/ -name *.sh | sort
 echo ""
 echo "You might find one of the sample config files useful:"
-find ./config/ -name *.properties
+find ./config/ -name *.properties | sort
 echo ""
 echo "Add more using volumes, or downstream images."
 echo "Enjoy Kafka!"
